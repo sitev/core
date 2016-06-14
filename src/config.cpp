3cc0da5a83985f8ae7aa3fc8d2f32e65666d366c
sitev <admin@sitev.ru>
2016-06-14 16:40:52 +0500

    .

--- src/config.cpp
+++ src/config.cpp
@@ -16,7 +16,6 @@ bool Config::loadFromFile(String fileName) {
 	while (!f->eof()) {
 		String s;
 		f->readLine(s);
-		string s1 = s.to_string();
 		bool flag = parse(s);
 		if (!flag) return false;
 	}
@@ -30,9 +29,9 @@ bool Config::parse(String s) {
 	if (pos < 0) return false;
 	String name = s.subString(0, pos);
 	String value = s.subString(pos + 1);
-	string n = name.to_string();
-	string v = value.to_string();
 	add(name, value);
+
+	return true;
 }
 
 
