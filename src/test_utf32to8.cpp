4ed4ea5a6369145bffd8641588ca5f82124ea0ee
sitev <admin@sitev.ru>
2016-12-11 20:29:51 +0500

    .

--- src/test_utf32to8.cpp
+++ src/test_utf32to8.cpp
@@ -19,10 +19,54 @@ int main(int argc, _TCHAR* argv[])
 			s += (char)(v & 0x7f);
 		}
 		else if (v <= 0x7ff) {
-			int a = 0xc0 + ((v & 0x7c0) >> 6);
-			int b = 0x80 + (v & 0x3f);
-			s += (char)a;
+			int a = 0x80 + (v & 0x3f);
+			int b = 0xc0 + ((v & 0x7c0) >> 6);
 			s += (char)b;
+			s += (char)a;
+		}
+		else if (v <= 0xffff) {
+			int a = 0x80 + (v & 0x3f);
+			int b = 0x80 + ((v & 0x0fc0) >> 6);
+			int c = 0xe0 + ((v & 0xf000) >> 12);
+			s += (char)c;
+			s += (char)b;
+			s += (char)a;
+		}
+		else if (v <= 0x1fffff) {
+			int a = 0x80 + (v & 0x3f);
+			int b = 0x80 + ((v & 0x0fc0) >> 6);
+			int c = 0x80 + ((v & 0x3f000) >> 12);
+			int d = 0xf0 + ((v & 0x1c0000) >> 18);
+			s += (char)d;
+			s += (char)c;
+			s += (char)b;
+			s += (char)a;
+		}
+		else if (v <= 0x03ffffff) {
+			int a = 0x80 + (v & 0x3f);
+			int b = 0x80 + ((v & 0x0fc0) >> 6);
+			int c = 0x80 + ((v & 0x3f000) >> 12);
+			int d = 0x80 + ((v & 0xfc0000) >> 18);
+			int e = 0xf8 + (v >> 24);
+			s += (char)e;
+			s += (char)d;
+			s += (char)c;
+			s += (char)b;
+			s += (char)a;
+		}
+		else {
+			int a = 0x80 + (v & 0x3f);
+			int b = 0x80 + ((v & 0x0fc0) >> 6);
+			int c = 0x80 + ((v & 0x3f000) >> 12);
+			int d = 0x80 + ((v & 0xfc0000) >> 18);
+			int e = 0x80 + ((v & 0x3f000000) >> 24);
+			int f = 0xfc + (v >> 30);
+			s += (char)f;
+			s += (char)e;
+			s += (char)d;
+			s += (char)c;
+			s += (char)b;
+			s += (char)a;
 		}
 	}
 	cout << s << endl;
